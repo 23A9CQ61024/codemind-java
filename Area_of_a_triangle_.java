727eceb2fb1eb8ede8a318ef4b3149c853dd5370
23A9CQ61024 <147476350+23A9CQ61024@users.noreply.github.com>
2024-08-03 12:08:09 +0530

    import java.util.Scanner; public class tri{     public static void main(String[] args)     {         int b,h;         Scanner read=new Scanner(System.in);         b=read.nextInt();         h=read.nextInt();         System.out.println(b*h/2);     } }

---
+++ Area_of_a_triangle_.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class tri{
+    public static void main(String[] args)
+    {
+        int b,h;
+        Scanner read=new Scanner(System.in);
+        b=read.nextInt();
+        h=read.nextInt();
+        System.out.println(b*h/2);
+    }
+}
